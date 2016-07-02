d2c6ff616840775179642aa20205d8d454787211
KentHan <kenthan.tw@gmail.com>
2016-07-03 00:45:37 +0800

    Update

--- moji_lyrics.py
+++ moji_lyrics.py
@@ -2,16 +2,35 @@
 
 import scrapy
 from pymongo import MongoClient
+from urlparse import urljoin
 
 
 class MojiLyricsSpider(scrapy.Spider):
     name = 'moji'
-    # start_urls = ['https://mojim.com/twh100092.htm'] # 張學友
-    start_urls = ['https://mojim.com/twh105643.htm'] # 石頭
 
+    # start_urls = ['https://mojim.com/twh105643.htm', 'https://mojim.com/twh100085.htm'] # 石頭 / 張宇
+    start_urls = ['https://mojim.com/twza.htm'] # 華語男生
+    
+    host = 'https://mojim.com'
+    
     client = MongoClient("mongodb://localhost:27017")
 
-    def parse(self, response):
+    ''' start from artist 
+    '''
+    # def parse(self, response):
+    #     for href in response.css('.hc3 a::attr(href)'):
+    #         full_url = response.urljoin(href.extract())[:-4] + ".html"
+    #         print(full_url)
+    #         yield scrapy.Request(full_url, callback=self.parse_song)
+
+    ''' start from list 
+    '''
+    def parse(self, response): # start from list
+        for href in response.css('.s_listA a::attr(href)'):
+            full_url = urljoin(self.host, href.extract())
+            yield scrapy.Request(full_url, callback=self.parse_artist)
+
+    def parse_artist(self, response):
         for href in response.css('.hc3 a::attr(href)'):
             full_url = response.urljoin(href.extract())[:-4] + ".html"
             print(full_url)
@@ -19,18 +38,57 @@ class MojiLyricsSpider(scrapy.Spider):
 
     def parse_song(self, response):
         artist = response.css('dl#fsZx1.fsZx1::text').extract_first().strip()
-        title = response.css('dt#fsZx2.fsZx2::text').extract_first()
-        lyrics = " ".join(response.css('dd#fsZx3.fsZx3::text').extract())
+        title = response.css('dt#fsZx2.fsZx2::text').extract_first().strip()
 
-        print(artist)
-        print(title)
-        print(lyrics)
+        contents = response.css('dd#fsZx3.fsZx3::text').extract()
+        lyrics = self.extract_lyrics(contents)
+        composer = self.extract_composer(contents)
+        lyricist = self.extract_lyricist(contents)
 
         db = self.client.test
-        db.lyrics.insert({'artist': artist, 'title': title, 'lyrics': lyrics})
+        db.lyrics.insert({'artist': artist, 'title': title, 'lyrics': lyrics, 'lyricist': lyricist, 'composer': composer})
 
         yield {
             'artist': artist,
             'title': title,
+            'lyricist': lyricist,
+            'composer': composer,
             'lyrics': lyrics
         }
+
+    def extract_lyrics(self, contents):
+        lyrics = ""
+        for line in contents:
+            if line.endswith(u'主題曲'):
+                pass
+            elif line.startswith(u'更多更詳盡歌詞'):
+                pass
+            elif line.startswith('Repeat') or line.startswith('REPEAT'):
+                pass
+            elif len(line.strip()) is 0:
+                pass
+            elif line.startswith('['):
+                pass
+            elif ":" in line or u"：" in line:
+                pass
+            else:
+                lyrics = lyrics + line + "\n"
+        return lyrics
+
+    def extract_composer(self, contents):
+        for line in contents:
+            if line.startswith(u'作曲：') or line.startswith(u'作曲:'):
+                return line[3:].strip()
+            elif  line.startswith(u'曲：') or line.startswith(u'曲:'):
+                return line[2:].strip()
+        return None
+
+    def extract_lyricist(self, contents):
+        for line in contents:
+            if line.startswith(u'作詞：') or line.startswith(u'作詞:'):
+                return line[3:].strip()
+            elif  line.startswith(u'詞：') or line.startswith(u'詞:'):
+                return line[2:].strip()
+        return None
+
+
